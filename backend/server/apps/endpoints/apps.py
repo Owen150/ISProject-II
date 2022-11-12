1ebd3f9c73fc3f8bd6631776b4e97e7308e843bb
Michael Owen Oduor <34193283+Owen150@users.noreply.github.com>
2022-11-12 04:08:48 +0300

    Updates

--- backend/server/apps/endpoints/apps.py
+++ backend/server/apps/endpoints/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 
 
-class EndpointsConfig(AppConfig):
-    name = 'endpoints'
+class EndpointsConfig(AppConfig):    
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.endpoints'
+    verbose_name = 'endpoints'
