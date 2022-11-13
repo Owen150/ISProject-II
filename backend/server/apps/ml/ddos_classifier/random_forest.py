608a3a0b9f6b5cad7ced77f9cdb31e3e25774a97
Michael Owen Oduor <34193283+Owen150@users.noreply.github.com>
2022-11-14 02:42:11 +0300

    Implemented the Random Forest ML algorithm code
    ML Code on the server side that will use the previously trained Random Forest Algorithm.

--- backend/server/apps/ml/ddos_classifier/random_forest.py
+++ backend/server/apps/ml/ddos_classifier/random_forest.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 class RandomForestClassifier:
     def __init__(self):
-        path_to_artifacts = "C:/Users/user/Desktop/DDoS-Botnet-Attack-detection-using-Random-Forest/notebooks/"
+        path_to_artifacts = "C:/Users/Michael Owen/OneDrive/Desktop/4.2/ISProject-II/notebooks/"
         self.values_fill_missing =  joblib.load(path_to_artifacts + "train_mode.joblib")
         self.encoders = joblib.load(path_to_artifacts + "encoders.joblib")
         self.model = joblib.load(path_to_artifacts + "random_forest.joblib")
@@ -12,7 +12,7 @@ class RandomForestClassifier:
     def preprocessing(self, input_data):
         # JSON to pandas DataFrame
         input_data = pd.DataFrame(input_data, index=[0])
-        # fill missing values
+        # Filling the missing values
         input_data.fillna(self.values_fill_missing)
         
         return input_data
@@ -23,13 +23,13 @@ class RandomForestClassifier:
     def postprocessing(self, input_data):
         label = "DDOS"
         if input_data[1] < 0.5:
-            label = "BENING"
+            label = "BENIGN"
         return {"probability": input_data[1], "label": label, "status": "OK"}
 
     def compute_prediction(self, input_data):
         try:
             input_data = self.preprocessing(input_data)
-            prediction = self.predict(input_data)[0]  # only one sample
+            prediction = self.predict(input_data)[0]  # Only One Sample
             prediction = self.postprocessing(prediction)
         except Exception as e:
             return {"status": "Error", "message": str(e)}
