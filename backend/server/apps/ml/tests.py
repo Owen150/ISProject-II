7b15f7c9668b74e625698965d13b63b078b2c16f
Michael Owen Oduor <34193283+Owen150@users.noreply.github.com>
2022-11-18 20:49:22 +0300

    Implemented a Second Test Case

--- backend/server/apps/ml/tests.py
+++ backend/server/apps/ml/tests.py
@@ -89,21 +89,20 @@ class MLTests(TestCase):
         self.assertEqual("OK", response["status"])
         self.assertTrue("label" in response)
         self.assertEqual("BENIGN", response["label"])
-	
-def test_registry(self):
-    registry = MLRegistry()
-    self.assertEqual(len(registry.endpoints), 0)
-    endpoint_name = "ddos_classifier"
-    algorithm_object = RandomForestClassifier()
-    algorithm_name = "random forest"
-    algorithm_status = "production"
-    algorithm_version = "0.0.1"
-    algorithm_owner = "Michael Owen"
-    algorithm_description = "Random Forest with simple pre and post processing"
-    algorithm_code = inspect.getsource(RandomForestClassifier)
-    # add to registry
-    registry.add_algorithm(endpoint_name, algorithm_object, algorithm_name,
-		algorithm_status, algorithm_version, algorithm_owner,
-		algorithm_description, algorithm_code)
-    # there should be one endpoint available
+    def test_registry(self):
+            registry = MLRegistry()
+            self.assertEqual(len(registry.endpoints), 0)
+            endpoint_name = "ddos_classifier"
+            algorithm_object = RandomForestClassifier()
+            algorithm_name = "random forest"
+            algorithm_status = "production"
+            algorithm_version = "0.0.1"
+            algorithm_owner = "Michael Owen"
+            algorithm_description = "Random Forest with simple pre- and post-processing"
+            algorithm_code = inspect.getsource(RandomForestClassifier)
+            # add to registry
+            registry.add_algorithm(endpoint_name, algorithm_object, algorithm_name,
+                        algorithm_status, algorithm_version, algorithm_owner,
+                        algorithm_description, algorithm_code)
+            # there should be one endpoint available
             self.assertEqual(len(registry.endpoints), 1)
