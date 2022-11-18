7b76c8ff8985bb10dd423c2d1d5220575e92a867
Michael Owen Oduor <34193283+Owen150@users.noreply.github.com>
2022-11-18 20:59:59 +0300

    Adding the ML algorithms to the registry

--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -14,3 +14,37 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
+
+# ML registry
+import inspect
+from apps.ml.registry import MLRegistry
+from apps.ml.ddos_classifier.random_forest import RandomForestClassifier
+from apps.ml.ddos_classifier.extra_trees import ExtraTreesClassifier # import ExtraTrees ML algorithm
+
+try:
+    registry = MLRegistry() # create ML registry
+    # Random Forest classifier
+    rf = RandomForestClassifier()
+    # add to ML registry
+    registry.add_algorithm(endpoint_name="ddos_classifier",
+                            algorithm_object=rf,
+                            algorithm_name="Random Forest",
+                            algorithm_status="production",
+                            algorithm_version="0.0.1",
+                            owner="Michael Owen",
+                            algorithm_description="Random Forest with simple pre- and post-processing",
+                            algorithm_code=inspect.getsource(RandomForestClassifier))
+
+    # Extra Trees classifier
+    et = ExtraTreesClassifier()
+    # add to ML registry
+    registry.add_algorithm(endpoint_name="ddos_classifier",
+                            algorithm_object=et,
+                            algorithm_name="Extra Trees",
+                            algorithm_status="testing",
+                            algorithm_version="0.0.1",
+                            owner="Michael Owen",
+                            algorithm_description="Extra Trees with simple pre- and post-processing",
+                            algorithm_code=inspect.getsource(ExtraTreesClassifier))
+except Exception as e:
+    print("Exception while loading the algorithms to the registry,", str(e))
